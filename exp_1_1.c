41449e423f5f68c10bcdd8087adcffe2e97e99d7
awez78 <shaikhawez7178@gmail.com>
2025-10-15 09:59:15 +0530

    Add files via upload

---
+++ exp_1_1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+
+    /*
+    NAME:- SHAIKH AWEZ SALLWUDIN
+    CLASS:- COMPS F.Y. DIVISION:- C-3
+    UNI:- 251P050
+    */
+
+    printf("Name = Shaikh Awez Sallwuddin\nAge = 19\nClass = F.Y. Comps\nDivision = C\nUIN = 251P050");//printed...!
+    return 0;
+}
