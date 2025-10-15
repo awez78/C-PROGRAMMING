64a652e7eed63b8c531cbf7bf5e8a661cc8250e0
awez78 <shaikhawez7178@gmail.com>
2025-10-15 10:07:15 +0530

    Add files via upload

---
+++ EXP_2_1.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+    /*
+    NAME:- SHAIKH AWEZ SALLWUDIN
+    CLASS:- COMPS F.Y. DIVISION:- C-3
+    UNI:- 251P050
+    */
+    int n;
+    printf("Enter the Number:");
+    scanf("%d",&n);
+    if(n%2==0)
+    {
+        printf("Entered Number is Even...!");
+
+    }
+    else
+    {
+        printf("Entered Number is Odd");
+    }
+    return 0;
+}
